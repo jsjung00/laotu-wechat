bb61e88f149283af9a0504665b4ea484a34ba31e
jsjung00 <justinsoljung@gmail.com>
2020-08-28 19:08:47 -0700

    working on itemCard component

--- miniprogram/components/itemCard/itemCard.js
+++ miniprogram/components/itemCard/itemCard.js
@@ -3,9 +3,21 @@ Component({
   /**
    * Component properties
    */
-  properties: {
-
+  options : {
+    multipleSlots : true,
+    virtualHost : true, //allows the component to grow to the container of the parent (parent container has display: flex)
+    styleIsolation : 'apply-shared' //parent page can change the CSS
   },
+   properties: {
+    imageSrc : {
+      type: String
+    },
+    style: {
+      type: String //see virtualHost documentation. Allows parent to pass in style through databinding
+      //<itemCard style=""></itemcard>
+    }
+  },
+  externalClasses : ['class'],
 
   /**
    * Component initial data
