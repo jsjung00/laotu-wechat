8194d2552b7dbe1d2f04e8a9b30755fddb3d96cb
jsjung00 <justinsoljung@gmail.com>
2020-08-31 13:51:24 -0700

    added above and below slot to itemCard

--- miniprogram/components/itemCard/itemCard.js
+++ miniprogram/components/itemCard/itemCard.js
@@ -13,6 +13,13 @@ Component({
       type: String,
       value: "https://www.jessicagavin.com/wp-content/uploads/2019/02/carrots-7-1200.jpg"
     },
+    isAbove : {
+      type: Boolean //determines whether or not there is an above slot on top of the card
+    },
+    isBelow: {
+      type: Boolean //determines whether or not there is a below slot
+    }
+
     /*style: {
       type: String //see virtualHost documentation. Allows parent to pass in style through databinding
       //<itemCard style=""></itemcard>
